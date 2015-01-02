e7413265d97648c3fbc9b54cc3b06b5bebac92ca
Andrea Morettin <divino.marchese@gmail.com>
2015-01-02 14:33:21 +0100

    150102 creted

---
+++ glob_loc.py
@@ -0,0 +1,12 @@
+#print(globals()) # prints global namespace
+#print(locals()) # prints local namespace
+
+glob = 1
+
+def foo():
+    loc = 5
+    print('loc in foo():', 'loc' in locals())
+
+foo()
+print('loc in global:', 'loc' in globals())    
+print('glob in global:', 'foo' in globals())
---
+++ skeletons.py
@@ -0,0 +1,18 @@
+class stack(object):
+    def __init__(self):
+        self.l = [None, None, None, None ....
+        self.top = 0
+        
+    def push(self, val):
+    
+    def pop(self):
+    
+        return ..
+        
+    
+    
+    def __str__(self): 
+        
+
+class Node(object)
+    
